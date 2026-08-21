5a6cabed1336e83b74846163607a938f4cf59848
1to1solutions <timothyeldred@1to1solutions.com>
2026-08-21 09:18:55 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 
 // Bumped on every build. A new build gets fresh caches and the old ones are
 // deleted on activate, so nobody is ever served yesterday's page.
-var BUILD = '1f0c70';
+var BUILD = '0db7ee';
 var SHELL = 'ch-shell-' + BUILD;
 var DOCS  = 'ch-docs-' + BUILD;
 
